2dbbfd8c09fd3df4fcfa8b4e5d564ee757def123
stephen-pinto <stephen_pinto@outlook.com>
2021-08-21 02:13:50 +0530

    Improved it to base solely on expressions

---
+++ ModelMapper/ModelMapper/ModelMapperNext.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace ModelMapper
+{
+    public class ModelMapperNext<SrcType, DstType>
+        where SrcType : class
+        where DstType : class
+    {
+        private Dictionary<string, object> defaults;
+        private List<Expression> assignmentExpressions = new List<Expression>();
+        private ParameterExpression srcInstance = null;
+        private ParameterExpression dstInstance = null;
+        private Action<SrcType, DstType> ConverterFunction { get; set; }
+
+        public ModelMapperNext()
+        {
+            defaults = new Dictionary<string, object>();
+
+            //Declare parameters for the converter
+            srcInstance = Expression.Parameter(typeof(SrcType), "srcObj");
+            dstInstance = Expression.Parameter(typeof(DstType), "dstObj");
+        }
+
+        public ModelMapperNext<SrcType, DstType> Add<ResType>(Expression<Func<SrcType, ResType>> expr1, Expression<Func<DstType, ResType>> expr2, object defaultValue = null)
+        {
+            if (ConverterFunction != null)
+                ConverterFunction = null;
+
+            string targetMember = GetMemberName(expr2);
+
+            var leftProp = Expression.Property(dstInstance, targetMember);
+            var rightProp = GetAppropriateExpression(expr1, typeof(ResType));
+
+            assignmentExpressions.Add(Expression.Assign(leftProp, rightProp));
+
+            return this;
+        }
+
+        public ModelMapperNext<SrcType, DstType> Build()
+        {
+            var block = Expression.Block(assignmentExpressions.ToArray());
+            var lamda = Expression.Lambda<Action<SrcType, DstType>>(block, new ParameterExpression[] { srcInstance, dstInstance });
+            ConverterFunction = lamda.Compile();
+            return this;
+        }
+
+        public void Clear()
+        {
+            assignmentExpressions.Clear();
+            defaults.Clear();
+        }
+
+        public void CopyChanges(SrcType sourceObj, DstType destinationObj)
+        {
+            if (ConverterFunction == null)
+                throw new InvalidOperationException("Build not called");
+
+            ConverterFunction(sourceObj, destinationObj);
+        }
+
+        public DstType GetNew(SrcType srcObj)
+        {
+            DstType dstObj = Activator.CreateInstance<DstType>();
+            CopyChanges(srcObj, dstObj);
+            return dstObj;
+        }
+
+        private Expression GetAppropriateExpression<T1>(Expression<T1> sourceExpression, Type resultType)
+        {
+            if (sourceExpression.Body is MemberExpression membrExpr)
+            {
+                string srcMember = GetMemberName(sourceExpression);
+                return Expression.Property(srcInstance, srcMember);
+            }
+            else if (sourceExpression.Body is UnaryExpression unaryExpr)
+            {
+                return Expression.Property(srcInstance, ((MemberExpression)unaryExpr.Operand).Member.Name);
+            }
+            else if (sourceExpression.Body is ConstantExpression constant)
+            {
+                return constant;
+            }
+            else
+            {
+                //Since we allow only one param type explicit indexing should be fine
+                var lamdaExprn = Expression.Lambda<Func<SrcType, object>>(sourceExpression.Body, sourceExpression.Parameters[0]);
+                var invokeExpr = Expression.Invoke(lamdaExprn, new Expression[] { srcInstance });
+                return Expression.Convert(invokeExpr, resultType);
+            }
+        }
+
+        private string GetMemberName<T>(Expression<T> expression)
+        {
+            var targetExpr = (MemberExpression)((expression.Body is MemberExpression) ? expression.Body : ((UnaryExpression)expression.Body).Operand);
+            return targetExpr.Member.Name;
+        }
+    }
+}
--- ModelMapper/TestApp/ModelMapperBasicTest.cs
+++ ModelMapper/TestApp/ModelMapperBasicTest.cs
@@ -1,6 +1,7 @@
 ﻿using ModelMapper;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,14 @@ namespace TestApp
     {
         public void Run()
         {
+            Test1();
+            Test2();
+        }
+
+        private void Test1()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+
             ModelOne x = new ModelOne()
             {
                 A = 5,
@@ -21,23 +30,66 @@ namespace TestApp
                 E = 11.10M
             };
 
-            ModelTwo y = new ModelTwo() 
-            { 
-                N = "fasss" 
+            ModelTwo y = new ModelTwo()
+            {
+                N = "fasss"
             };
 
             Console.WriteLine($"Before changes: \nX:\n{x} \nY:\n{y}\n");
+            stopwatch.Start();
 
             ModelMapper<ModelOne, ModelTwo> umapper = new ModelMapper<ModelOne, ModelTwo>();
+
             umapper.Add(f => f.A, t => t.M);
             //umapper.Add(f => "hello there", t => t.N);
             umapper.Add(() => "hello there", t => t.N);
             //umapper.Add(f => f.C.ToString(), t => t.N);
             umapper.Add(f => f.C, t => t.O);
+            umapper.Add(f => f.D, t => t.P);
             umapper.Add(f => f.E, t => t.Q);
-            umapper.CopyChanges(x, y);
 
-            Console.WriteLine($"After changes: \nY:\n{y}\n");
+            for (int i = 0; i < 7000; i++)
+                umapper.CopyChanges(x, y);
+
+            stopwatch.Stop();
+            Console.WriteLine($"\nAfter changes: \nY:\n{y}\n in {stopwatch.Elapsed.TotalSeconds} secs\n\n");
+        }
+
+        private void Test2()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+
+            ModelOne x = new ModelOne()
+            {
+                A = 5,
+                B = "dasdasd",
+                C = false,
+                D = 12.1521,
+                E = 11.10M
+            };
+
+            ModelTwo y = new ModelTwo()
+            {
+                N = "fasss"
+            };
+
+            Console.WriteLine($"Before changes: \nX:\n{x} \nY:\n{y}\n");
+            stopwatch.Start();
+
+            ModelMapperNext<ModelOne, ModelTwo> umapper = new ModelMapperNext<ModelOne, ModelTwo>();
+
+            umapper.Add(f => f.A, t => t.M)
+                .Add(f => "Hello there", t => t.N)
+                .Add(f => f.C, t => t.O)
+                .Add(f => f.D, t => t.P)
+                .Add(f => f.E, t => t.Q)
+                .Build();
+
+            for (int i = 0; i < 7000; i++)
+                umapper.CopyChanges(x, y);
+
+            stopwatch.Stop();
+            Console.WriteLine($"\nAfter changes: \nY:\n{y}\n in {stopwatch.Elapsed.TotalSeconds} secs\n\n");
         }
     }
 }
